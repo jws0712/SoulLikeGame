f6b5d03722ae77dca5983051f64c36fa25e63879
jws2170 <mpart2077@gmail.com>
2024-07-07 23:49:19 +0900

    [ADD] PlayerRotate

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,58 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Assertions.Must;
-
-public class Player : MonoBehaviour
+namespace SOUL.Player
 {
-    [SerializeField] float WalkSpeed;
-    [SerializeField] float sprintSpeed;
-    private float moveSpeed;
-    private Rigidbody rb;
-    private Animator anim;
-    private Vector3 dir;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
 
-        moveSpeed = WalkSpeed;
-    }
+    //System
+    using System.Collections;
+    using System.Collections.Generic;
+    using Unity.VisualScripting;
 
-    // Update is called once per frame
-    void Update()
+    //UnityEngine
+    using UnityEngine;
+    using UnityEngine.Assertions.Must;
+    
+    public class Player : MonoBehaviour
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        [SerializeField] float WalkSpeed;
+        [SerializeField] float sprintSpeed;
+        [SerializeField] float smoothTime = 0.05f;
+       
+
+        //private variable
+        private float moveSpeed;
+        private Rigidbody rb;
+        private Animator anim;
+        private Vector3 dir;
+        private float currentVelocity;
+
+
+        private void Start()
         {
-            moveSpeed = sprintSpeed;
-            Debug.Log("¶Ü");
-        }
-        else
-        {
+            rb = GetComponent<Rigidbody>();
+            anim = GetComponent<Animator>();
+
             moveSpeed = WalkSpeed;
-            Debug.Log("°É¾î");
         }
-
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        dir = (Vector3.forward * vertical + Vector3.right * horizontal).normalized;
-
-        transform.LookAt(transform.position + dir);
-
-        if(dir != Vector3.zero)
+        private void Update()
         {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            if (horizontal == 0f && vertical == 0f)
+            {
+                dir = Vector3.zero;
+                anim.SetBool("IsMove", false);
+                moveSpeed = 0f;
+                anim.SetFloat("MoveSpeed", moveSpeed);
+                return;
+            } 
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                moveSpeed = sprintSpeed;
+            }
+            else
+            {
+                moveSpeed = WalkSpeed;
+            }
+
+            dir = (Vector3.forward * vertical + Vector3.right * horizontal).normalized;
+
+            var targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, smoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
             anim.SetBool("IsMove", true);
-        }
-        else
-        {
-            anim.SetBool("IsMove", false);
+            anim.SetFloat("MoveSpeed", moveSpeed);
         }
 
-        anim.SetFloat("MoveSpeed", moveSpeed);
-    }
-    private void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + dir * moveSpeed * Time.deltaTime);
+
+        private void FixedUpdate()
+        {
+            rb.MovePosition(rb.position + dir * moveSpeed * Time.deltaTime);
+        }
     }
 }
+

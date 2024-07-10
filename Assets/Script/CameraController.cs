322e0b480224418552e24ed3076d9d76922bb53b
jws2170 <mpart2077@gmail.com>
2024-07-10 20:39:56 +0900

    [Fix] Shadow

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMove : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
     [SerializeField] private float rotSensitive = 3f;
     [SerializeField] private float dis = 5f;
     [SerializeField] private float RotationMin = -10f;
     [SerializeField] private float RotationMax = 80f;
     [SerializeField] private float smoothTime = 0.12f;
-    [SerializeField] private Transform target;
+
+    public Transform target;
 
     private float Yaxis;
     private float Xaxis;
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,6 +11,9 @@ namespace SOUL.Player
     //UnityEngine
     using UnityEngine;
     using UnityEngine.Assertions.Must;
+
+    //Project
+
     
     public class Player : MonoBehaviour
     {
@@ -18,8 +21,11 @@ namespace SOUL.Player
         [SerializeField] private float WalkSpeed = default;
         [SerializeField] private float sprintSpeed = default;
         [SerializeField] private float spinSpeed = default;
+        [Header("PlayerSetting")]
+        [SerializeField] private float sensDistance = default;
+        [SerializeField] private LayerMask sensLayer = default;
         [Header("PlayerCameraSetting")]
-        [SerializeField] private Camera mainCamera = default;
+        [SerializeField] private GameObject mainCamera = default;
 
 
        
@@ -29,6 +35,7 @@ namespace SOUL.Player
         private float currentVelocity;
         private float targetAngle;
         private float angle;
+        private bool isSens;
 
         private Rigidbody rb;
         private Animator anim;
@@ -51,6 +58,13 @@ namespace SOUL.Player
         {
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
+
+            LookingEnemy();
+
+            if (isSens)
+            {
+                PlayerRotate(dir);
+            }
 
             if (horizontal == 0f && vertical == 0f)
             {
@@ -79,7 +93,6 @@ namespace SOUL.Player
             }
 
             Vector3 forward = mainCamera.transform.forward;
-
             Vector3 right = mainCamera.transform.right;
 
             forward.y = 0f;
@@ -112,6 +125,51 @@ namespace SOUL.Player
             float angle = Mathf.SmoothDampAngle(transform.localEulerAngles.y, targetAngle, ref currentVelocity, spinSpeed);
 
             transform.localRotation = Quaternion.Euler(0, angle, 0);
+        }
+
+        /// <summary>
+        /// 적을 쳐다보는 함수
+        /// </summary>
+        private void LookingEnemy()
+        {
+            Collider[] enemys = Physics.OverlapSphere(transform.position, sensDistance, sensLayer);
+
+            if(enemys.Length > 0 )
+            {
+                float nearDistance = Vector3.Distance(transform.position, enemys[0].transform.position);
+                Collider nearEnemy = enemys[0];
+
+                foreach (Collider col in enemys)
+                {
+                    float distacne = Vector3.Distance(transform.position, col.transform.position);
+
+                    if (distacne < nearDistance)
+                    {
+                        nearDistance = distacne;
+                        nearEnemy = col;
+                    }
+                }
+                Debug.Log(nearEnemy.name);
+
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    if (isSens == false)
+                    {
+                        isSens = true;
+                        mainCamera.transform.LookAt(nearEnemy.transform);
+                        Debug.Log("야 보라고");
+                    }
+                }
+            }
+
+
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(this.transform.position, sensDistance);
         }
     }
 }
